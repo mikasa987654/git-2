d47048ebc224743c17e0233240cce5a16d02d71c
mikasa987654 <141610191+mikasa987654@users.noreply.github.com>
2023-08-08 23:37:35 +0800

    Update test.java

--- src/dada/git/test.java
+++ src/dada/git/test.java
@@ -8,5 +8,6 @@ public class test {
         System.out.println("hello git3");
         System.out.println("hello git3");
         System.out.println("hello git3");
+        System.out.println("hello git5555555555555");
     }
 }
